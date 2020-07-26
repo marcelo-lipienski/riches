315fe014ae7bf13c17bd2d584490c043d02d6d80
Marcelo Lipienski <marcelo.lipienski@gmail.com>
2020-07-26 17:32:30 -0300

    (+) Adds password_digest column to users table (+) Adds bcrypt password encryptation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
+
   enum gender: { female: 0, male: 1 }
 
   validates :fullname, :document, :birthdate, :gender, :password, presence: true
