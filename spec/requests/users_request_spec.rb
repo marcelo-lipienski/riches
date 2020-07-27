3ff38a82b7e64f05c4d5dfe87ff4a05db3c28a7a
Marcelo Lipienski <marcelo.lipienski@gmail.com>
2020-07-26 22:26:54 -0300

    User address
    In order to register an user, they must provide address information
    
    (+) Adds `has_one/belongs_to` relationship between User and Address
    (+) Adds `UserRegistrationService` to extract logic from `UsersController#create`
    (+) Adds `UserRegistrationService` test
    (=) Updates `UsersControllers#create` test to add address dependency

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -4,19 +4,53 @@ require('rails_helper')
 
 RSpec.describe('Users', type: :request) do
   describe('POST /') do
-    let(:user) { attributes_for(:user) }
+    let(:user)    { attributes_for(:user) }
+    let(:address) { attributes_for(:address) }
 
-    it('returns http created') do
-      post('/users', params: user, as: :json)
-      expect(response).to(have_http_status(:created))
+    let(:invalid_user) do
+      # Sets all user attributes to empty string
+      user.each { |k, _v| user[k] = '' }
     end
 
-    it('returns http internal server error') do
-      # Sets all user attributes to empty string
-      invalid_user = user.each { |k, _v| user[k] = '' }
+    let(:invalid_address) do
+      # Sets all address attributes to empty string
+      address.each { |k, _v| address[k] = '' }
+    end
 
-      post('/users', params: invalid_user, as: :json)
-      expect(response).to(have_http_status(:internal_server_error))
+    context('when user and address are valid') do
+      it('returns http created') do
+        params = user.merge(address: address)
+
+        post('/users', params: params, as: :json)
+        expect(response).to(have_http_status(:created))
+      end
+    end
+
+    context('when user is invalid and address is valid') do
+      it('returns http internal server error') do
+        params = invalid_user.merge(address: address)
+
+        post('/users', params: params, as: :json)
+        expect(response).to(have_http_status(:internal_server_error))
+      end
+    end
+
+    context('when user is valid and address is invalid') do
+      it('returns http internal server error') do
+        params = user.merge(address: invalid_address)
+
+        post('/users', params: params, as: :json)
+        expect(response).to(have_http_status(:internal_server_error))
+      end
+    end
+
+    context('when user and address are invalid') do
+      it('returns http internal server error') do
+        params = invalid_user.merge(address: invalid_address)
+
+        post('/users', params: params, as: :json)
+        expect(response).to(have_http_status(:internal_server_error))
+      end
     end
   end
 end
